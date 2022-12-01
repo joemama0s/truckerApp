d8b0deb778c506d8088dcf816492399c107969fd
Tyler Quast <tylerquast68@gmail.com>
2022-11-30 19:52:09 -0700

    login and sign up work

--- trucker-app-next/src/pages/login.tsx
+++ trucker-app-next/src/pages/login.tsx
@@ -12,75 +12,42 @@ export default function Login() {
   const passwordRef = useRef<HTMLInputElement>(null);
   const verificationRef = useRef<HTMLInputElement>(null);
   const [enteringCode, setEnteringCode] = useState<boolean>(false);
+  const [errorString, setErrorString] = useState<string>("");
   const { user, setUser } = useUser();
-  // TODO Fix red underline
-  // function checkEmployeeLogin() {
-  //   setUser(usernameRef?.current?.value);
-  //   saveProfileType("employer");
-  //   router.push("/employer_profile/" + usernameRef?.current?.value);
-  // }
 
-  // function checkDriverLogin() {
-  //   saveUsername(usernameRef?.current?.value);
-  //   saveProfileType("driver");
-  //   router.push("/driver_profile/" + usernameRef?.current?.value);
-  // }
+  function getErrorMessage(error: unknown) {
+    if (error instanceof Error) return error.message;
+    return String(error);
+  }
+
+  const reportError = ({ message }: { message: string }) => {
+    setErrorString(message);
+  };
+
   async function submitSignIn() {
     try {
-      if (enteringCode) {
-        await verify();
-      } else {
-        await signUp();
-        setEnteringCode(true);
-      }
+      await submitLogin();
     } catch (err) {
       console.error(err);
+      reportError({ message: getErrorMessage(err) });
     }
   }
 
-  async function signUp(): Promise<CognitoUser> {
+  // TODO USE THIS FOR MORE OPTIMIZED ERROR LOGGING
+  // throw new Error("Please Select Profile Type");
+  async function submitLogin() {
     try {
-      const username = usernameRef?.current?.value
+      const usernameValue = usernameRef?.current?.value
         ? usernameRef?.current?.value
         : "";
-      const password = passwordRef?.current?.value
+      const passwordValue = passwordRef?.current?.value
         ? passwordRef?.current?.value
         : "";
-      // TODO add email input
-      const email = "kekadi1147@teknowa.com";
-
-      const { user } = await Auth.signUp({
-        username,
-        password,
-        attributes: {
-          email: email, // optional
-        },
-        autoSignIn: {
-          // optional - enables auto sign in after user is    confirmed
-          enabled: true,
-        },
-      });
-      console.log(user);
-      console.log("Successfully added user");
-      return user;
-    } catch (error) {
-      console.log("error signing up:", error);
-      throw error;
-    }
-  }
-
-  async function verify() {
-    const username = "poopoopeepee";
-    const password = "joemama23";
-    const code = verificationRef?.current?.value
-      ? verificationRef?.current?.value
-      : "";
-    try {
-      await Auth.confirmSignUp(username, code);
-      const amplifyUser = await Auth.signIn(username, password);
+      const amplifyUser = await Auth.signIn(usernameValue, passwordValue);
       console.log("Success! Verified User");
     } catch (error) {
       console.log("Error verifying: ", error);
+      reportError({ message: getErrorMessage(error) });
     }
   }
 
@@ -93,67 +60,29 @@ export default function Login() {
       <h1 className="text-3xl text-yellow-400 font-bold underline">
         Login Page!
       </h1>
-      <p>
-        Use ID 1 or 2 for username to go to a profile. All others will redirect,
-        password not considered
-      </p>
-      {!enteringCode ? (
-        <div className="container">
-          <div className="bg-blue-300 text-center w-1/3 px-3 py-4 text-white-100 mx-auto rounded">
-            <input
-              type="text"
-              placeholder="Username"
-              className="block w-full mx-auto text-sm py-2 px-3 rounded"
-              ref={usernameRef}
-            />
-            <input
-              type="text"
-              placeholder="Password NOT CONSIDERED"
-              className="block w-full mx-auto text-sm py-2 px-3 rounded my-3"
-              ref={passwordRef}
-            />
-            <button
-              onClick={() => {
-                console.log("BUTTON CLICKED");
-              }}
-              className="bg-blue-300 text-white-100 font-bold py-2 px-4 rounded border block mx-auto w-full"
-            >
-              Sign in as driver
-            </button>
-            <button
-              onClick={() => {
-                console.log("BUTTON CLICKED");
-              }}
-              className="bg-blue-300 text-white-100 font-bold py-2 px-4 rounded border block mx-auto w-full"
-            >
-              Sign in as employer
-            </button>
-            <button
-              onClick={submitSignIn}
-              className="bg-blue-300 text-white-100 font-bold py-2 px-4 rounded border block mx-auto w-full"
-            >
-              Sign Up
-            </button>
-          </div>
+      <p>DEBUG: useername: tylerquast password: joemama23</p>
+      <div className="container">
+        <div className="bg-blue-300 text-center w-1/3 px-3 py-4 text-white-100 mx-auto rounded">
+          <input
+            type="text"
+            placeholder="Username"
+            className="block w-full mx-auto text-sm py-2 px-3 rounded"
+            ref={usernameRef}
+          />
+          <input
+            type="text"
+            placeholder="Password NOT CONSIDERED"
+            className="block w-full mx-auto text-sm py-2 px-3 rounded my-3"
+            ref={passwordRef}
+          />
+          <button
+            onClick={submitSignIn}
+            className="bg-blue-300 text-white-100 font-bold py-2 px-4 rounded border block mx-auto w-full"
+          >
+            Sign in
+          </button>
         </div>
-      ) : (
-        <div className="container">
-          <div className="bg-blue-300 text-center w-1/3 px-3 py-4 text-white-100 mx-auto rounded">
-            <input
-              type="text"
-              placeholder="Verification Code"
-              className="block w-full mx-auto text-sm py-2 px-3 rounded"
-              ref={verificationRef}
-            />
-            <button
-              onClick={submitSignIn}
-              className="bg-blue-300 text-white-100 font-bold py-2 px-4 rounded border block mx-auto w-full"
-            >
-              Submit Verify
-            </button>
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
